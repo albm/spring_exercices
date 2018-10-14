313c44253f1783c174735d617f09c529bf6372f7
albm <abascu@gmail.com>
2018-10-14 16:21:42 +0200

    Simple advertisment app

---
+++ src/main/java/eads/upm/es/Anuncio.java
@@ -0,0 +1,12 @@
+package eads.upm.es;
+
+public class Anuncio {
+	
+	public String nombre, asunto, texto;
+	
+	public Anuncio(String nombre2, String asunto2, String texto2) {
+			nombre = nombre2;
+			asunto = asunto2;
+			texto = texto2;
+	}
+}
